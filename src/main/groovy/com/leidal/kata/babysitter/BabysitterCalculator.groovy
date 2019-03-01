672cfb57316bad5477b7a9eb98da176601574cc4
Luke Leidal <lleidal7@gmail.com>
2019-02-28 19:19:33 -0600

    Babysitter cannot end after 4 AM

--- src/main/groovy/com/leidal/kata/babysitter/BabysitterCalculator.groovy
+++ src/main/groovy/com/leidal/kata/babysitter/BabysitterCalculator.groovy
@@ -1,10 +1,14 @@
 package com.leidal.kata.babysitter
 
+
 import java.time.LocalTime
 
-
 class BabysitterCalculator {
-    BigDecimal calculateTotalPay(LocalTime startTime) throws Exception{
-        throw new Exception("Babysitter cannot start before 5 PM")
+    BigDecimal calculateTotalPay(LocalTime startTime, LocalTime endTime) throws Exception{
+        if(startTime.isBefore(LocalTime.of(17, 00))) {
+            throw new Exception("Babysitter cannot start before 5 PM")
+        } else {
+            throw new Exception("Babysitter cannot end after 4 AM")
+        }
     }
 }
--- src/test/groovy/com/leidal/kata/babysitter/BabysitterCalculatorTest.groovy
+++ src/test/groovy/com/leidal/kata/babysitter/BabysitterCalculatorTest.groovy
@@ -9,8 +9,16 @@ class BabysitterCalculatorTest extends GroovyTestCase {
     void testShouldNotStartEarlierThan5PM() {
         babysitterCalculator = new BabysitterCalculator()
         def msg = shouldFail {
-            BigDecimal pay = babysitterCalculator.calculateTotalPay(LocalTime.of(16, 59))
+            babysitterCalculator.calculateTotalPay(LocalTime.of(16, 59), LocalTime.of(23, 59))
         }
         assert 'Babysitter cannot start before 5 PM' == msg
+    }
+
+    void testShouldNotEndLaterThan4AM() {
+        babysitterCalculator = new BabysitterCalculator()
+        def msg = shouldFail {
+            babysitterCalculator.calculateTotalPay(LocalTime.of(17,00), LocalTime.of(4, 01))
+        }
+        assert 'Babysitter cannot end after 4 AM' == msg
     }
 }
