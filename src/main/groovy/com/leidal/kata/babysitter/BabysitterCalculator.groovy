f9f72d5a56b3d4fa5eef25d64b4470fd0ad47d07
Luke Leidal <lleidal7@gmail.com>
2019-03-04 20:49:18 -0600

    Babysitter cannot end before starting

--- src/main/groovy/com/leidal/kata/babysitter/BabysitterCalculator.groovy
+++ src/main/groovy/com/leidal/kata/babysitter/BabysitterCalculator.groovy
@@ -6,12 +6,23 @@ import java.time.LocalTime
 
 class BabysitterCalculator {
     BigDecimal calculateTotalPay(LocalDateTime startDateTime, LocalDateTime endDateTime, Family family) throws Exception{
-        if(startDateTime.toLocalTime().isBefore(LocalTime.of(17,00))) {
+        if(family == null) {
+            throw new Exception("Babysitter must have a family for which to babysit")
+        } else (
+            validateHoursAreAcceptable(startDateTime, endDateTime)
+        )
+
+    }
+
+    void validateHoursAreAcceptable(LocalDateTime startDateTime, LocalDateTime endDateTime) {
+        if(endDateTime.isBefore(startDateTime)) {
+            throw new Exception("End time must be after start time")
+        } else if(startDateTime.toLocalTime().isBefore(LocalTime.of(17,00))) {
             throw new Exception("Babysitter cannot start before 5 PM")
-        } else if(endDateTime.toLocalTime().isAfter(LocalTime.of(4,00))){
+        } else if(endDateTime.toLocalTime().isAfter(LocalTime.of(4,00))) {
             throw new Exception("Babysitter cannot end after 4 AM")
         } else {
-            throw new Exception("Babysitter must have a family for which to babysit")
+            //hopefully we can actually pay the babysitter
         }
     }
 }
--- src/test/groovy/com/leidal/kata/babysitter/BabysitterCalculatorTest.groovy
+++ src/test/groovy/com/leidal/kata/babysitter/BabysitterCalculatorTest.groovy
@@ -16,8 +16,9 @@ class BabysitterCalculatorTest extends GroovyTestCase {
         def msg = shouldFail {
             babysitterCalculator.calculateTotalPay(
                     LocalDateTime.of(2019, 2, 28, 16, 59),
-                    LocalDateTime.of(2019, 2, 28, 0, 0),
-                    new Family())
+                    LocalDateTime.of(2019, 2, 28, 20, 0),
+                    new Family()
+            )
         }
         assert 'Babysitter cannot start before 5 PM' == msg
     }
@@ -26,8 +27,9 @@ class BabysitterCalculatorTest extends GroovyTestCase {
         def msg = shouldFail {
             babysitterCalculator.calculateTotalPay(
                     LocalDateTime.of(2019, 2, 28, 17,00),
-                    LocalDateTime.of(2019, 3, 01, 4, 01),
-                    new Family())
+                    LocalDateTime.of(2019, 3, 1, 4, 01),
+                    new Family()
+            )
         }
         assert 'Babysitter cannot end after 4 AM' == msg
     }
@@ -40,5 +42,16 @@ class BabysitterCalculatorTest extends GroovyTestCase {
                     null)
         }
         assert 'Babysitter must have a family for which to babysit' == msg
+    }
+
+    void testShouldNotHaveEndTimeBeforeStartTime() {
+        def msg = shouldFail {
+            babysitterCalculator.calculateTotalPay(
+                    LocalDateTime.of(2019, 02, 28, 17, 00),
+                    LocalDateTime.of(2019, 02, 28, 16, 59),
+                    new Family()
+            )
+        }
+        assert 'End time must be after start time' == msg
     }
 }
